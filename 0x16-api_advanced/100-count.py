53c8776af576e8c3c28b5848ef896bca7a8aaa7f
bchen528 <bchen720@gmail.com>
2018-08-31 05:45:00 +0000

    remove flag and change order of after assignment

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -7,7 +7,7 @@ from collections import OrderedDict
 from requests import get
 
 
-def count_words(subreddit, word_list, after=None, match_dict={}, flag=0):
+def count_words(subreddit, word_list, after=None, match_dict={}):
     """parses the title of all hot articles, and prints a sorted count of given
     keywords (case-insensitive, delimited by spaces)
     Args:
@@ -17,15 +17,32 @@ def count_words(subreddit, word_list, after=None, match_dict={}, flag=0):
         match_dict (dict): dictionary of frequency of words from word_list
         flag (int): indicate when match_dict is complete
     """
-    try:
-        r = get('https://www.reddit.com/r/{}/hot.json?limit=100&&'
-                'after={}'.format(subreddit, after),
-                headers={'User-Agent': 'bc'})
-        sub_dict = r.json()
+    r = get('https://www.reddit.com/r/{}/hot.json?limit=100&&'
+            'after={}'.format(subreddit, after),
+            headers={'User-Agent': 'bc'})
+    sub_dict = r.json()
 
-        if after is None and match_dict != {}:
+    try:
+        if match_dict == {}:
+            for w in word_list:
+                match_dict[w] = 0
+
+        after = sub_dict['data']['after']
+
+        for i in range(len(sub_dict['data']['children'])):
+            title_string = sub_dict['data']['children'][i]['data']['title']
+            search_list = title_string.split()
+            for word in search_list:
+                for w in word_list:
+                    if w.lower() == word.lower():
+                        match_dict[w] += 1
+
+        if after is not None:
+            count_words(subreddit, word_list, after, match_dict)
+        else:
             descend_dict = OrderedDict(sorted(match_dict.items(),
-                                       key=lambda x: x[1], reverse=True))
+                                              key=lambda x: x[1],
+                                              reverse=True))
             zero_count = 0
             for k, v in descend_dict.items():
                 if v != 0:
@@ -34,22 +51,6 @@ def count_words(subreddit, word_list, after=None, match_dict={}, flag=0):
                     zero_count += 1
             if zero_count == len(descend_dict):
                 print()
-            flag = 1
-
-        if match_dict == {}:
-            for w in word_list:
-                match_dict[w] = 0
-
-        if flag == 0:
-            for i in range(len(sub_dict['data']['children'])):
-                title_string = sub_dict['data']['children'][i]['data']['title']
-                search_list = title_string.split()
-                for word in search_list:
-                    for w in word_list:
-                        if w.lower() == word.lower():
-                            match_dict[w] += 1
-            after = sub_dict['data']['after']
-            count_words(subreddit, word_list, after, match_dict)
 
     except:
         print()
