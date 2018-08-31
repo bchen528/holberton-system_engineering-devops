4ff7368d2c2c7d9ee766857b1280155f1114747e
bchen528 <bchen720@gmail.com>
2018-08-31 04:28:38 +0000

    alphabetize modules

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -3,8 +3,8 @@
 parses the title of all hot articles, and prints a sorted count of given
 keywords (case-insensitive, delimited by spaces)
 """
-from requests import get
 from collections import OrderedDict
+from requests import get
 
 
 def count_words(subreddit, word_list, after=None, match_dict={}, flag=0):
