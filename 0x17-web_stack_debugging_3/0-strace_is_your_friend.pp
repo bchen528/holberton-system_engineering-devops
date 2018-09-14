0bf5b9ff2315bb7f1b1ee6c3106f05a9c62db864
bchen528 <bchen720@gmail.com>
2018-09-14 01:20:01 +0000

    puppet-lint 2.1.1 compliant

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # fix 500 error
 exec {'fix500':
-  command  => 'sed -i "s|require_once( ABSPATH . WPINC . \'/class-wp-locale.phpp\' );\
+  command => 'sed -i "s|require_once( ABSPATH . WPINC . \'/class-wp-locale.phpp\' );\
 |require_once( ABSPATH . WPINC . \'/class-wp-locale.php\' );|g" /var/www/html/wp-settings.php',
-      path => '/bin/:/sbin/:/usr/bin/:/usr/sbin/'
+  path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/'
 }
